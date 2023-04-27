376e63a3bfeb124fb7df9ed10894f232cc3a5427
Patrick Beal <patrick.beal@take.net>
2023-04-27 18:32:20 -0300

    try

--- chrome/script.js
+++ chrome/script.js
@@ -1,26 +1,31 @@
-const ERROR_BUTTON_CLASS_SELECTOR = "ScCoreButtonDestructive";
+const ERROR_BUTTON_CLASS_SELECTOR = 'ScCoreButtonDestructive';
 const VIDEO_PLAYER_CLASS_SELECTOR = ".video-player__overlay";
+const LEFT_PLAYER_CONTROL_CLASS_SELECTOR = "player-controls__left-control-group";
+let pollsMade = 0;
 
-function findReloadButtonAndClickIt() {
-  const reloadButton = document.querySelector(
-    `button[class*="${ERROR_BUTTON_CLASS_SELECTOR}"]`
-  );
-  if (!!reloadButton) {
-    console.log("Error detected! Reloading player...");
-    reloadButton.click();
-  }
+const pollForReloadButton = () => {
+  setTimeout(() => {
+    const errorButton = document.querySelector(`button[class*="${ERROR_BUTTON_CLASS_SELECTOR}"]`);
+    const videoPlayer = document.querySelector(VIDEO_PLAYER_CLASS_SELECTOR);
+    const pausedButton = videoPlayer?.querySelector('button[data-a-player-state*="paused"]');
+    const leftPlayerControl = document.querySelector(`div[class*="${LEFT_PLAYER_CONTROL_CLASS_SELECTOR}"]`);
+
+    console.log('Current try: ' + pollsMade);
+
+    if (errorButton && !leftPlayerControl) {
+      if(pollsMade > 5) {
+        console.log('Max tries reached. Exiting...')
+        return;
+      }
+      pollsMade++;
+      errorButton.click();
+    } else if(!errorButton && leftPlayerControl) {
+      pollsMade = 0;
+    }
+
+    pollForReloadButton();
+  }, 1000);
 }
+console.log('[LOCAL]')
 
-function detectPlaybackError() {
-  const videoPlayer = document.querySelector(VIDEO_PLAYER_CLASS_SELECTOR);
-  if (!!videoPlayer) {
-    console.log("Twitch player found. \nError detection initialized.");
-    const observer = new MutationObserver(findReloadButtonAndClickIt);
-    observer.observe(videoPlayer, { childList: true, subtree: true });
-  } else {
-    console.log("Player not found. \nRetrying...");
-    setTimeout(detectPlaybackError, 500);
-  }
-}
-
-detectPlaybackError();
+pollForReloadButton();
