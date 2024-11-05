c0e842ebb7a791ab41a891794ba1f1441becdf87
Patrick <patrickvrb@gmail.com>
2024-11-04 22:25:34 -0300

    fix: Updated script

--- chrome/script.js
+++ chrome/script.js
@@ -1,26 +1,22 @@
-const ERROR_BUTTON_CLASS_SELECTOR = 'ScCoreButtonDestructive';
-const TRANSITION_STATE_CLASS_SELECTOR = ".player-overlay-background";
 const MAX_RELOAD_TRIES = 5;
 const POLL_FREQUENCY = 1000;
 let pollsMade = 0;
 
 const pollForReloadButton = () => {
   setTimeout(() => {
-    const errorButton = document.querySelector(`button[class*="${ERROR_BUTTON_CLASS_SELECTOR}"]`);
-    const isTransitionState = !!document.querySelector(TRANSITION_STATE_CLASS_SELECTOR)
+    const errorOverlay = document.querySelector('div[data-a-target="player-overlay-content-gate"]')
+    const isTransitionState = !!document.querySelector(".player-overlay-background");
 
-    if (errorButton) {
-      if(!errorButton.querySelector(`div[class*="ScFigure"]`)){
-        return;
-      }
-      if(pollsMade >= MAX_RELOAD_TRIES) {
+    if (errorOverlay) {
+      if (pollsMade >= MAX_RELOAD_TRIES) {
         console.log('Max tries reloading player reached.')
         return;
       }
       pollsMade++;
       console.log('Error detected. Reloading player...')
+      const errorButton = errorOverlay.querySelector('button');
       errorButton.click();
-    } else if(!errorButton && !isTransitionState) { // Player running with no errors
+    } else if (!isTransitionState) { // Player running with no errors
       pollsMade = 0;
     }
 
@@ -28,5 +24,6 @@ const pollForReloadButton = () => {
   }, POLL_FREQUENCY);
 }
 
-console.log('Twitch player auto-reload initialized.')
+
+console.log('Twitch player auto-reload initialized. 123')
 pollForReloadButton();
